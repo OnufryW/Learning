c965be1b9ef24af0fbe13414c9affd0fb75091f0
Onufry Wojtaszczyk <onufryw@gmail.com>
2016-09-17 10:58:56 +0200

    fdafa

--- bosch/training/network.py
+++ bosch/training/network.py
@@ -3,24 +3,31 @@ import numpy as np
 import tensorflow as tf
 import sys
 import random
+import argparse
 
-keep_rate = float(sys.argv[1]) if len(sys.argv) > 1 else 0.9
-depth = int(sys.argv[2]) if len(sys.argv) > 2 else 2
-width = int(sys.argv[3]) if len(sys.argv) > 3 else 1024
-filename = sys.argv[4] if len(sys.argv) > 4 else "DefaultGraph"
+parser = argparse.ArgumentParser(description='Create and init the graph.')
+parser.add_argument('-d', '--depth', default=2, type=int,
+        help='Number of network layers')
+parser.add_argument('-w', '--width', default=1024, type=int,
+        help='Width of a single layer')
+parser.add_argument('-k', '--keep_rate', default=0.9, type=float,
+        help='Keep ratio of dropout layers')
+parser.add_argument('filename', help='Filename for the graph')
+
+args = parser.parse_args()
 
 input_size=1936 
 sdev=0.01
 
-assert depth > 1
-assert keep_rate > 0
-assert keep_rate <= 1
+assert args.depth > 1
+assert args.keep_rate > 0
+assert args.keep_rate <= 1
 
-with open(filename + ".log", "w") as logfile:
-  logfile.write("%s %f %d %d\n" % (filename, keep_rate, depth, width))
+with open(args.filename + ".log", "w") as logfile:
+  logfile.write("%s %f %d %d\n" % (args.filename, args.keep_rate, args.depth, args.width))
 
 seed = random.randint(0, 2 ** 30)
-with open(filename + ".config", "w") as configfile:
+with open(args.filename + ".config", "w") as configfile:
   configfile.write(str(seed) + "\n")
 
 graph = tf.Graph()
@@ -33,29 +40,29 @@ with graph.as_default():
   biases = []
 
   weights.append(tf.Variable(
-      tf.truncated_normal([input_size, width], stddev=sdev), name="weights1"))
+      tf.truncated_normal([input_size, args.width], stddev=sdev), name="weights1"))
   biases.append(tf.Variable(
-      tf.truncated_normal([width], stddev=sdev), name="biases1"))
-  for x in range(depth - 2):
+      tf.truncated_normal([args.width], stddev=sdev), name="biases1"))
+  for x in range(args.depth - 2):
     weights.append(tf.Variable(tf.truncated_normal(
-        [width, width], stddev=sdev), name="weights"+str(x+2)))
+        [args.width, args.width], stddev=sdev), name="weights"+str(x+2)))
     biases.append(tf.Variable(tf.truncated_normal(
-        [width], stddev=sdev), name="biases"+str(x+2)))
+        [args.width], stddev=sdev), name="biases"+str(x+2)))
   weights.append(tf.Variable(
-      tf.truncated_normal([width, 1], stddev=sdev), name="weights"+str(depth)))
+      tf.truncated_normal([args.width, 1], stddev=sdev), name="weights"+str(args.depth)))
   biases.append(tf.Variable(
-      tf.truncated_normal([1], stddev=sdev), name="biases"+str(depth)))
+      tf.truncated_normal([1], stddev=sdev), name="biases"+str(args.depth)))
 
   # Let's do dropout on training to prevent overfitting.
   def construct_network(dropout):
     suffix = "D" if dropout else ""
     state = input_dataset
-    for x in range(depth-1):
+    for x in range(args.depth-1):
       operated = tf.matmul(
               state, weights[x], name="base"+str(x)+suffix) + biases[x]
       neuron = tf.nn.relu(operated, name="neuron"+str(x)+suffix)
       if dropout:
-        state = tf.nn.dropout(neuron, keep_rate)
+        state = tf.nn.dropout(neuron, args.keep_rate)
       else:
         state = neuron
     return tf.matmul(state, weights[-1], name="final"+suffix) + biases[-1]
@@ -79,10 +86,10 @@ with graph.as_default():
   optimizer = tf.train.AdamOptimizer().minimize(loss, name="Opt")
 
   clean_logits = tf.reshape(construct_network(False), [-1], name="Pred")
-  tf.train.export_meta_graph(filename=(filename + ".meta"))
+  tf.train.export_meta_graph(filename=(args.filename + ".meta"))
 
 with tf.Session(graph=graph) as session:
   saver = tf.train.Saver(tf.all_variables())
   tf.initialize_all_variables().run()
   print('Initialized')
-  saver.save(session, filename)
+  saver.save(session, args.filename)

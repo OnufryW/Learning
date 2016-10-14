269c12db4fe65565ec866e762f4983f1ab8b6320
Onufry Wojtaszczyk <onufryw@gmail.com>
2016-10-14 22:00:32 +0200

    Translate Andrej's pong to Tensorflow, attach Tensorboard.

--- pong/rewrite/pong.py
+++ pong/rewrite/pong.py
@@ -15,8 +15,8 @@ parser.add_argument('--width', help='Width of hidden layer', default=200,
 parser.add_argument('--gamma', help='Reward discount factor over time',
         default=0.99, type=float)
 parser.add_argument('--decay_rate', help='Decay rate of gradient memory',
-        default='0.99', type=float)
-parser.add_argument('--learning_rate', help='Learning rate', default=1e-4,
+        default='0.95', type=float)
+parser.add_argument('--learning_rate', help='Learning rate', default=0.0001,
         type=float)
 parser.add_argument('--logdir', help='Base directory for logs', default='/tmp')
 parser.add_argument('--batch_size', help='Number of games between learning',
@@ -44,7 +44,7 @@ def add_var_summaries(var, name):
   with tf.name_scope("summaries"):
     mean = tf.reduce_mean(var)
     tf.scalar_summary(name + "/mean", mean)
-    stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+    stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)), name="sumstdv")
     tf.scalar_summary(name + "/stddev", stddev)
     tf.scalar_summary(name + "/max", tf.reduce_max(var))
     tf.scalar_summary(name + "/min", tf.reduce_min(var))
@@ -59,7 +59,7 @@ def weight_relu(inp, w_shape, layer_name, should_relu):
     add_var_summaries(weights, layer_name + "/weights")
     biases = tf.Variable(tf.truncated_normal([w_shape[1]], stddev=0.1))
     add_var_summaries(biases, layer_name + "/biases")
-    op = tf.matmul(inp, weights) + biases
+    op = tf.add(tf.matmul(inp, weights), biases)
     if should_relu:
       add_var_summaries(op, layer_name + "/pre-activate")
       neuron = tf.nn.relu(op)
@@ -75,16 +75,10 @@ def discount_rewards(r):
     if r[t]: running_add = 0 # Reset sum, since at game boundary.
     running_add = running_add * args.gamma + r[t]
     discounted_r[t] = running_add
-  return discounted_r
-
-def set_labels(rewards, results, choices):
-  discounted = discount_rewards(rewards)
   # Now, normalize.
-  discounted -= np.mean(discounted)
-  discounted /= np.std(discounted)
-  vresults = np.vstack(results).ravel()
-  vchoices = np.vstack(choices).ravel()
-  return vresults + (vchoices - vresults) * discounted
+  discounted_r -= np.mean(discounted_r)
+  discounted_r /= np.std(discounted_r)
+  return discounted_r
 
 graph = tf.Graph()
 with graph.as_default():
@@ -95,14 +89,25 @@ with graph.as_default():
   end_val = tf.sigmoid(result)
   add_var_summaries(end_val, "Probabilities")
 
-  # We add the rewards for the sole purpose of exporting them in TB.
+  # We add the placeholders for the sole purpose of exporting them in TB.
   rewards_ph = tf.placeholder(tf.float32)
   add_var_summaries(rewards_ph, "Rewards")
-  labels = tf.placeholder(tf.float32)
-  loss = tf.reduce_mean(end_val - labels)
-  tf.scalar_summary("loss", loss)
-  optimizer = tf.train.AdadeltaOptimizer(learning_rate=args.learning_rate,
-          rho=args.decay_rate, epsilon=1e-5).minimize(loss)
+  discounted_ph = tf.placeholder(tf.float32)
+  add_var_summaries(discounted_ph, "DiscountedRewards")
+  choices_ph = tf.placeholder(tf.float32)
+  add_var_summaries(choices_ph, "Choices")
+
+  # The real learning part.
+  loss_entries = (choices_ph - end_val) * discounted_ph
+  add_var_summaries(loss_entries, "Loss")
+  loss = tf.reduce_sum(loss_entries)
+  optimizer = tf.train.GradientDescentOptimizer(learning_rate=args.learning_rate)
+#  optimizer = tf.train.AdadeltaOptimizer(learning_rate=args.learning_rate,
+#          rho=args.decay_rate)
+  grads = optimizer.compute_gradients(loss)
+  for grad in grads:
+    add_var_summaries(grad[0], "GradientOf" + grad[1].name)
+  minimizer = optimizer.apply_gradients(grads)
   merged = tf.merge_all_summaries()
   init = tf.initialize_all_variables()
   log_writer = tf.train.SummaryWriter(args.logdir + "/pong", graph)
@@ -111,13 +116,14 @@ with graph.as_default():
   inputs, choices, results, rewards = [], [], [], []
   prev_input = None
   observation = env.reset()
-  running_reward = None
+  running_reward = -21.
   reward_sum = 0
   episode_number = 0
   to_take = 0
 
   session = tf.Session()
   with session.as_default():
+    # print [x.name for x in graph.get_operations()]
     session.run(init)
     while True:
       if args.render: env.render()
@@ -129,10 +135,8 @@ with graph.as_default():
       up_prob = session.run([end_val],
                             feed_dict={input_image : inp.reshape((1,D))})[0]
 
-      # Now, we should probably not ignore the observation, but whatever.
-      action = 2 if np.random.uniform() < up_prob else 3
+      action = 2 if np.random.uniform() > up_prob else 3
       choices.append(action - 2)
-      results.append(up_prob)
       inputs.append(inp)
       observation, reward, done, info = env.step(action)
       reward_sum += reward
@@ -140,8 +144,7 @@ with graph.as_default():
 
       if done:
         # Bookkeep
-        running_reward = reward_sum if running_reward is None else (
-                running_reward * 0.99 + reward_sum * 0.01)
+        running_reward = running_reward * 0.99 + reward_sum * 0.01
         print 'resetting env. episode reward total was %f. running mean %f' % (
                 reward_sum, running_reward)
         reward_sum = 0
@@ -149,24 +152,28 @@ with graph.as_default():
         episode_number += 1
 
         # Let's export statistics.
+        xx = graph.get_tensor_by_name('FinalLayer/summaries/Max:0')
         run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
         run_metadata = tf.RunMetadata()
-        summaries, _, _ = session.run([merged, end_val, loss],
-                feed_dict = {input_image : np.vstack(inputs[to_take:]),
-                             rewards_ph : np.vstack(
-                                 x for x in rewards[to_take:] if x),
-                             labels : set_labels(rewards[to_take:],
-                                 results[to_take:], choices[to_take:])},
+        summaries, _, _, x = session.run([merged, end_val, loss, xx],
+                feed_dict = {
+                    input_image : np.vstack(inputs[to_take:]),
+                    rewards_ph : np.vstack([x for x in rewards[to_take:] if x]),
+                    choices_ph : np.vstack(choices[to_take:]),
+                    discounted_ph : discount_rewards(rewards[to_take:])},
                 options=run_options,
                 run_metadata=run_metadata)
+        print x
         log_writer.add_run_metadata(run_metadata, 'Epis. %d' % episode_number)
         log_writer.add_summary(summaries, episode_number)
+        to_take = len(rewards)
 
         # Learn.
         if (episode_number + 1) % args.batch_size == 0:
-          session.run([optimizer],
-                  feed_dict={input_image : np.vstack(inputs),
-                             labels : set_labels(rewards, results, choices)})
+          feed_dict = {input_image: np.vstack(inputs),
+                       choices_ph : np.vstack(choices),
+                       discounted_ph : discount_rewards(rewards)}
+          session.run(minimizer, feed_dict=feed_dict)
           inputs, choices, results, rewards = [], [], [], []
           to_take = 0
 
